dec398fe2eba70184db1b0d920e381038a8836b1
nitsu0 <sorceofnext@gmail.com>
2020-04-18 19:28:28 +0900

    Finish create page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,21 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save!
+    redirect_to posts_url, notice: "「#{post.name}」を投稿しました。"
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :description)
   end
 end
